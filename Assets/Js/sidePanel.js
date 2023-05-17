16af867e02005d28fde184eccc73e1b289dd39cd
Vinicius Luz <112346721+ViniciusLuz99@users.noreply.github.com>
2023-05-16 22:24:35 -0300

    Mudanças e melhoras na responsividade

--- Assets/Js/sidePanel.js
+++ Assets/Js/sidePanel.js
@@ -1,8 +1,29 @@
-/* Set the width of the sidebar to 300px (show it) */
+// /* Set the width of the sidebar to 300px (show it) */
+// function openNav() {
+//   document.getElementById("mySidepanel").style.width = "300px";
+// }
+// /* Set the width of the sidebar to 0 (hide it) */
+// function closeNav() {
+//   document.getElementById("mySidepanel").style.width = "0";
+// }
+/*Codigo antigo acima, novo abaixo*/
+/**
+ * Abre o painel lateral (menu).
+ * Se o painel já estiver aberto, fecha-o.
+ * Se estiver fechado, abre-o definindo a largura como 300px.
+ */
 function openNav() {
-  document.getElementById("mySidepanel").style.width = "300px";
+  var sidepanel = document.getElementById("mySidepanel");
+  if (sidepanel.style.width === "300px") {
+    closeNav(); // Chama a função closeNav() se o painel já estiver aberto.
+  } else {
+    sidepanel.style.width = "300px"; // Abre o painel definindo a largura como 300px.
+  }
 }
-/* Set the width of the sidebar to 0 (hide it) */
+
+/**
+ * Fecha o painel lateral (menu) definindo a largura como 0.
+ */
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
 }
